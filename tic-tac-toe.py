3dd52d9fd61c56f1c89fbe42a2cbb8c19c379a97
akashyadav <akashyadav256526@gmail.com>
2019-06-10 14:32:10 +0530

    adding graphic to matrix

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,37 +1,48 @@
-mtrix= [x for x in range(9)]
-print(mtrix)
+#declared a list to represent matrix
+matrixtrix= [x for x in range(9)]
+print(matrixtrix)
+print('player 1 will be zero')
+p1=input('Please enter name of player 1')
+p2=input('Please enter name of player 2')
+name_and_symbol={
+	'O':p1,
+	'X':p2
+}
+//method to mark the positon chosed by player
 def mark_position(player,pos):
 	while True:
-		if isinstance(mtrix[pos], int):
-			mtrix[pos]=player
+		#check if the position is already used or not
+		if isinstance(matrix[pos], int):
+			matrix[pos]=player
 			return False
 		else:
 			print("please input an position wich is not used!")
 			return True
+#method to choose who is the winner 
 def check_if_winner(lst):
 	if lst[0]==lst[1]==lst[2]:
-		print('{} is the winner'.format(lst[0]))
+		print('{} is the winner'.format(name_and_symbol[0]))
 		return False
 	if lst[0]==lst[3]==lst[6]:
-		print('{} is the winner'.format(lst[0]))
+		print('{} is the winner'.format(name_and_symbol[0]))
 		return False
 	if lst[0]==lst[4]==lst[8]:
-		print('{} is the winner'.format(lst[0]))
+		print('{} is the winner'.format(name_and_symbol[0]))
 		return False
 	if lst[6]==lst[7]==lst[8]:
-		print('{} is the winner'.format(lst[6]))
+		print('{} is the winner'.format(name_and_symbol[6]))
 		return False
 	if lst[2]==lst[5]==lst[8]:
-		print('{} is the winner'.format(lst[2]))
+		print('{} is the winner'.format(name_and_symbol[2]))
 		return False
 	if lst[2]==lst[4]==lst[6]:
-		print('{} is the winner'.format(lst[2]))
+		print('{} is the winner'.format(name_and_symbol[2]))
 		return False
 	if lst[3]==lst[4]==lst[5]:
-		print('{} is the winner'.format(lst[3]))
+		print('{} is the winner'.format(name_and_symbol[4]))
 		return False
 	if lst[1]==lst[4]==lst[7]:
-		print('{} is the winner'.format(lst[1]))
+		print('{} is the winner'.format(name_and_symbol[4]))
 		return False
 	return True
 print('_____________')
@@ -44,12 +55,16 @@ for x in range(9):
 	while chosed:
 		print('the square are denoted by numbers')
 		pos=int(input("player 1 please choose a square"))
-		chosed= mark_position('p1',pos)
+		chosed= mark_position('O',pos)
 	chosed= True
 	while chosed:
 		print('the square are denoted by numbers')
 		pos=int(input("player 2 please choose a square"))
-		chosed= mark_position('p2',pos)
+		chosed= mark_position('X',pos)
 	chosed= True
-	if not check_if_winner(mtrix):
+	print('_____________')
+	print('|_{}_|_{}_|_{}_|'.format(matrix))
+	print('|_{}_|_{}_|_{}_|'.format(matrix))
+	print('|_{}_|_{}_|_{}_|'.format(matrix))	
+	if not check_if_winner(matrix):
 		break
