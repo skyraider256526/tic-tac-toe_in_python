e382ab88748a69e1f741ad0839902ded819737d3
akashyadav <akashyadav256526@gmail.com>
2019-06-10 14:43:32 +0530

    up and running V1.0

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,6 +1,6 @@
 #declared a list to represent matrix
-matrixtrix= [x for x in range(9)]
-print(matrixtrix)
+matrix= [x for x in range(9)]
+print(matrix)
 print('player 1 will be zero')
 p1=input('Please enter name of player 1')
 p2=input('Please enter name of player 2')
@@ -8,7 +8,8 @@ name_and_symbol={
 	'O':p1,
 	'X':p2
 }
-//method to mark the positon chosed by player
+
+#method to mark the positon chosed by player
 def mark_position(player,pos):
 	while True:
 		#check if the position is already used or not
@@ -21,50 +22,52 @@ def mark_position(player,pos):
 #method to choose who is the winner 
 def check_if_winner(lst):
 	if lst[0]==lst[1]==lst[2]:
-		print('{} is the winner'.format(name_and_symbol[0]))
+		print('{} is the winner'.format(name_and_symbol[lst[0]]))
 		return False
 	if lst[0]==lst[3]==lst[6]:
-		print('{} is the winner'.format(name_and_symbol[0]))
+		print('{} is the winner'.format(name_and_symbol[lst[0]]))
 		return False
 	if lst[0]==lst[4]==lst[8]:
-		print('{} is the winner'.format(name_and_symbol[0]))
+		print('{} is the winner'.format(name_and_symbol[lst[0]]))
 		return False
 	if lst[6]==lst[7]==lst[8]:
-		print('{} is the winner'.format(name_and_symbol[6]))
+		print('{} is the winner'.format(name_and_symbol[lst[8]]))
 		return False
 	if lst[2]==lst[5]==lst[8]:
-		print('{} is the winner'.format(name_and_symbol[2]))
+		print('{} is the winner'.format(name_and_symbol[lst[8]]))
 		return False
 	if lst[2]==lst[4]==lst[6]:
-		print('{} is the winner'.format(name_and_symbol[2]))
+		print('{} is the winner'.format(name_and_symbol[lst[4]]))
 		return False
 	if lst[3]==lst[4]==lst[5]:
-		print('{} is the winner'.format(name_and_symbol[4]))
+		print('{} is the winner'.format(name_and_symbol[lst[4]]))
 		return False
 	if lst[1]==lst[4]==lst[7]:
-		print('{} is the winner'.format(name_and_symbol[4]))
+		print('{} is the winner'.format(name_and_symbol[lst[4]]))
 		return False
 	return True
 print('_____________')
-print('|_1_|_2_|_3_|')
-print('|_4_|_5_|_6_|')
-print('|_7_|_8_|_9_|')
+print('|_0_|_1_|_2_|')
+print('|_3_|_4_|_5_|')
+print('|_6_|_7_|_8_|')
 chosed= True
 for x in range(9):
 	print(x,'iteration')
+	print('_____________')
+	print('|_{}_|_{}_|_{}_|'.format(matrix[0],matrix[1],matrix[2]))
+	print('|_{}_|_{}_|_{}_|'.format(matrix[3],matrix[4],matrix[5]))
+	print('|_{}_|_{}_|_{}_|'.format(matrix[6],matrix[7],matrix[8]))
 	while chosed:
 		print('the square are denoted by numbers')
 		pos=int(input("player 1 please choose a square"))
 		chosed= mark_position('O',pos)
 	chosed= True
+	if not check_if_winner(matrix):
+		break
 	while chosed:
 		print('the square are denoted by numbers')
 		pos=int(input("player 2 please choose a square"))
 		chosed= mark_position('X',pos)
-	chosed= True
-	print('_____________')
-	print('|_{}_|_{}_|_{}_|'.format(matrix))
-	print('|_{}_|_{}_|_{}_|'.format(matrix))
-	print('|_{}_|_{}_|_{}_|'.format(matrix))	
+	chosed= True	
 	if not check_if_winner(matrix):
 		break
